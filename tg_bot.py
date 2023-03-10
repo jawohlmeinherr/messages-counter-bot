e2d9f78740ef1c56b808c2afe41a2baf36424baf
Oleg Kukuruzo <jawohlmeinherr@protonmail.com>
2023-03-10 18:45:11 +0300

    New commands

--- tg_bot.py
+++ tg_bot.py
@@ -18,15 +18,38 @@ sql.execute(""" CREATE TABLE IF NOT EXISTS count (
 db.commit()
 
 # echo
-
 @bot.message_handler(commands=['echo'])
 def echo(message):
-    msg = bot.reply_to(message, '1')
-    bot.register_next_step_handler(msg, echo_txt)
+    if message.from_user.id in users_whitelist:
+        msg = bot.reply_to(message, 'Перешли сообщение, объект которого нужно напечатать в консоль!')
+        bot.register_next_step_handler(msg, echo_txt)
+    else:
+        bot.reply_to(message, 'У тебя нет прав для использования этой команды. Сорян!')
 
 def echo_txt(message):
     print(message)
     bot.reply_to(message, 'Выведено в консоль!')
+
+# add new admin
+@bot.message_handler(commands=['add_new_admin'])
+def receive_new_admin_id(message):
+    if message.from_user.id in users_whitelist:
+        msg = bot.reply_to(message, 'Перешли сообщение от того человека, которого хочешь назначить администратором бота!')
+        bot.register_next_step_handler(msg, add_new_admin)
+    else:
+        bot.reply_to(message, 'У тебя нет прав для использования этой команд. Сорян!')
+
+def add_new_admin(message):
+    users_whitelist.append(message.forward_from.id)
+    bot.reply_to(message, f'Добавлен новый администратор с id {message.forward_from.id}')
+
+#list if admins
+@bot.message_handler(commands=['list_of_admins'])
+def list_of_admins(message):
+    if message.from_user.id in users_whitelist:
+        bot.reply_to(message, f'Список текущих администраторов: {users_whitelist}')
+    else:
+        bot.reply_to(message, 'У тебя нет прав для использования этой команды. Сорян!')
 
 # check user statistics
 @bot.message_handler(commands=['check_user'])
