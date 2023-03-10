633ff1225f54df3f5398048c6aa32ed388eadc5e
Oleg Kukuruzo <jawohlmeinherr@protonmail.com>
2023-03-10 19:24:39 +0300

    Final commit (lmao)

--- tg_bot.py
+++ tg_bot.py
@@ -1,8 +1,9 @@
+# Created from podval with love
+# Author: github.com/jawohlmeinherr
+
 from telebot import types
 import telebot
 import sqlite3
-
-users_whitelist = [1619385320, 600204669]
 
 token = '6199924141:AAE9yarKSlNi-8wiVp6-aFX51E3ASKGZsCw'
 
@@ -20,7 +21,15 @@ db.commit()
 # echo
 @bot.message_handler(commands=['echo'])
 def echo(message):
-    if message.from_user.id in users_whitelist:
+    sql.execute("""SECELT * FROM admins""")
+    r = sql.fetchall()
+
+    amdins_ids = []
+
+    for k in r:
+        admins_ids.append(k[0])
+
+    if str(message.from_user.id) in admins_ids:
         msg = bot.reply_to(message, 'Перешли сообщение, объект которого нужно напечатать в консоль!')
         bot.register_next_step_handler(msg, echo_txt)
     else:
@@ -33,28 +42,53 @@ def echo_txt(message):
 # add new admin
 @bot.message_handler(commands=['add_new_admin'])
 def receive_new_admin_id(message):
-    if message.from_user.id in users_whitelist:
+    sql.execute("""SELECT * FROM admins""")
+    r = sql.fetchall()
+
+    admins_ids = []
+
+    for k in r:
+        admins_ids.append(k[0])
+
+    if str(message.from_user.id) in admins_ids:
         msg = bot.reply_to(message, 'Перешли сообщение от того человека, которого хочешь назначить администратором бота!')
         bot.register_next_step_handler(msg, add_new_admin)
     else:
         bot.reply_to(message, 'У тебя нет прав для использования этой команд. Сорян!')
 
 def add_new_admin(message):
-    users_whitelist.append(message.forward_from.id)
+    new_admin_id = str(message.forward_from.id)
+    sql.execute("""INSERT INTO admins VALUES (?)""", (new_admin_id,))
     bot.reply_to(message, f'Добавлен новый администратор с id {message.forward_from.id}')
 
 #list if admins
 @bot.message_handler(commands=['list_of_admins'])
 def list_of_admins(message):
-    if message.from_user.id in users_whitelist:
-        bot.reply_to(message, f'Список текущих администраторов: {users_whitelist}')
+    sql.execute("""SELECT * FROM admins""")
+    r = sql.fetchall()
+
+    admins_ids = []
+
+    for k in r:
+        admins_ids.append(k[0])
+
+    if str(message.from_user.id) in admins_ids:
+        bot.reply_to(message, f'Список текущих администраторов: {admins_ids}')
     else:
         bot.reply_to(message, 'У тебя нет прав для использования этой команды. Сорян!')
 
 # check user statistics
 @bot.message_handler(commands=['check_user'])
 def request_user_message(message):
-    if message.from_user.id in users_whitelist:
+    sql.execute("""SELECT * FROM admins""")
+    r = sql.fetchall()
+
+    admins_ids = []
+
+    for k in r:
+        admins_ids.append(k[0])
+
+    if str(message.from_user.id) in admins_ids:
         msg = bot.reply_to(message, 'Перешли сообщение человека, статистику по которому ты хочешь увидеть!')
         bot.register_next_step_handler(msg, check_user_statistics)
     else:
@@ -67,6 +101,30 @@ def check_user_statistics(message):
         bot.reply_to(message, f'user_id: {r[0][0]}\nmessages: {r[0][1]}\nsymbols:{r[0][2]}')
     else:
         bot.reply_to(message, 'Этого пользователя нет в базе данных!')
+
+# delete admin
+
+@bot.message_handler(commands=['delete_admin'])
+def request_admin_id(message):
+    sql.execute("""SELECT * FROM admins""")
+    r = sql.fetchall()
+
+    admins_ids = []
+
+    for k in r:
+        admins_ids.append(k[0])
+
+    if str(message.from_user.id) in admins_ids:
+        msg = bot.reply_to(message, 'Перешли сообщение человека, которого ты хочешь убрать из списка администраторов!')
+        bot.register_next_step_handler(msg, delete_admin)
+    else:
+        bot.reply_to(message, 'У тебя нет прав для использования этой команды. Сорян!')
+
+def delete_admin(message):
+    admin_id_needs_to_delete = str(message.forward_from.id)
+    sql.execute("""DELETE FROM admins WHERE admin_id = ?""", (admin_id_needs_to_delete,))
+    db.commit()
+    bot.reply_to(message, f'Администратор с id {admin_id_needs_to_delete} удален из списка администраторов!')
 
 # adding data to database
 
